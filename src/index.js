eb2efed4f00e500ff047241ce742c5498ac1942b
MariiaHurieva13 <Marygureva@gmail.com>
2025-06-24 09:33:38 +0200

    feat(hw3): add updated files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import dotenv from 'dotenv';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
 
-dotenv.config();
-
-try {
-  // Connect to MongoDB first
+const bootstrap = async () => {
   await initMongoConnection();
-  console.log('MongoDB connected successfully');
-  
-  // Then start the server
-  await setupServer();
-} catch (error) {
-  console.error('Failed to start application:', error);
-  process.exit(1);
-}
+  setupServer();
+};
+
+bootstrap();
